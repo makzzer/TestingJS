6bc32b1cb7ff8056570ce72896542b706992b186
makzzer <103696636+makzzer@users.noreply.github.com>
2022-05-22 01:04:00 -0300

    check arrow functions voy por la mitad

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ function sumar(num1) {
   console.log(num1);
 }
 
-sumar(40);
+sumar(100);
 
 //Arrow Functions
 
@@ -18,13 +18,30 @@ const sumar2 = (num2) => {
 sumar2(80);
 
 //Reduccion de codigo
+
 //cuando tengo solo un parametro, puedo omitir los parentesis
 //ahora si son mas de un parametro si o si tengo que poner parentesis
 //cuando es solo uno , son opcionales
 
 //ejemplo con un parametro
 
-const sumar3 = (num3) => {
-  console.log(num3);
+const sumar3 = (num3) => console.log(num3);
+
+sumar3(90);
+
+//cuando tengo dos paremetros tengo que colocar si o si los parentesis
+
+const sumar4 = (num4, num5) => console.log(num4 + num5);
+
+sumar4(70, 8);
+
+//¿que pasa si queremos retornar algo? tenemos que guardarlo en
+//una constante donde se almacene
+
+const sumar5 = (num6, num7) => {
+  return num6 + num7;
 };
-sumar3(60);
+
+const resultado = sumar5(100, 200);
+
+console.log(resultado);
